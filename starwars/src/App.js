9577a949b9eb244d1718d0a027a4238f6827f8e1
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-12 13:04:00 -0400

    properly mapped data and now styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import Characters from "./Characters";
 
 const Base_URL = "https://swapi.dev/api/";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [cardState, setCardState] = useState({});
+  const [cardState, setCardState] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   useEffect(() => {
-    axios.get(`${Base_URL}people/1/`).then((Response) => {
-      console.log(Response);
-      setCardState(Response.data);
+    axios.get(`${Base_URL}people/`).then((res) => {
+      console.log(res);
+      setCardState(res.data.results);
     });
   }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <div>
-        {/* <Characters info={cardState} /> */}
-        <div>
-          <h2>{cardState.name}</h2>
-          <p> {cardState.birth_year}</p>
-        </div>
-      </div>
+      <h1 className="Header">StarWars Characters</h1>
+      {cardState?.map((post) => {
+        return <Characters {...post} />;
+      })}
     </div>
   );
 };
--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -1,8 +1,28 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  margin: 100px auto;
+  width: 800px;
+  border: 1px solid gray;
+  border-radius: 14px;
+`;
 
+const Name = styled.h2`
+  color: white;
+`;
 
-const ApodCard = ({ info }) => (
-    
+const BirthYear = styled.p`
+  color: white;
+`;
+
+const Characters = ({ name, birth_year }) => (
+  <Container>
+    <Name>{name}</Name>
+    <BirthYear>{birth_year}</BirthYear>
+  </Container>
 );
+export default Characters;
